6cfb96596825f54681486bbb7ef8ed5987304930
codedjay672 <emmajoel672@gmail.com>
2023-01-13 10:39:11 +0000

    update puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # puppet script that edits the ssh_config file
 
 exec { 'echo':
-  command => '/usr/bin/echo "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config'
+  command => '/usr/bin/echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config'
 }
