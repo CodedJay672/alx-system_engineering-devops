40c1eee1bb8177da714ba532fc24e60231dda97f
codedjay672 <emmajoel6456@gmail.com>
2023-03-01 11:50:47 +0100

    update api file

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+function that queries a reddit API and returns number of
+subscribers for a given subreddit
+
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """function for getting subreddit users"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            "User-Agent": 'alx-api_advanced'
+    }
+    params = {
+            "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print('None')
+    results = response.json().get('data')
+    [print(c.get('data').get('title')) for c in results.get('children')]
